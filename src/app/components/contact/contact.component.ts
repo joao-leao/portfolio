b89955645638699f51019eaf2ee4886923e9d3b2
joao-leao <flocoswhite@gmail.com>
2024-04-20 00:06:51 -0300

    feat: clipboard functionality

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,18 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
-  copyEmail: string = "flocoswhite@gmail.com";
-  copyTell: number = 62993305993;
+  check1: boolean = false;
+  copy1: boolean = true;
+  check2: boolean = false;
+  copy2: boolean = true;
   
   constructor() { }
 
   ngOnInit(): void {
+  }
+  async copiarEmail(){
+    try {
+      await navigator.clipboard.writeText("flocoswhite@gmail.com");
+      this.copy1 = false;
+      this.check1 = true;
+      setTimeout(() => {
+        this.check1 = false;
+        this.copy1 = true;
+      }, 2500);
+    } catch (err){
+      console.error("Error:", err);
+    }
+  }
+
+  async copiarNumero(){
+    try {
+      await navigator.clipboard.writeText("62993305993");
+      this.copy2 = false;
+      this.check2 = true;
+      setTimeout(() => {
+        this.check2 = false;
+        this.copy2 = true;
+      }, 2500);
+    } catch (err){
+      console.error("Error:", err);
+    }
   }
 
 }
