3265c2ce65d39c6a5e2e97d34d129be880bcb147
joao-leao <flocoswhite@gmail.com>
2024-04-26 21:43:41 -0300

    feat: responsive header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +8,45 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   activeSection: string | undefined;
 
+  isNavbarVisible: boolean = false;
+  buttonAnimation: boolean = false;
+
+  private clickOutsideListener: any;
+
   constructor() {}
 
   onSectionChange(sectionId: string) {
     this.activeSection = sectionId;
   }
-    
+
+  HiddenNav(){
+    this.isNavbarVisible = !this.isNavbarVisible;
+    this.buttonAnimation = !this.buttonAnimation;
+    if (this.isNavbarVisible) {
+      this.activateClickOutsideListener(); 
+    } else {
+      this.deactivateClickOutsideListener(); 
+    }
+  }
+
+  closeNavbar() {
+    this.isNavbarVisible = false;
+    this.buttonAnimation = false;
+    this.deactivateClickOutsideListener();
+  } 
   
+  onClick(event: MouseEvent) {
+      if (!(event.target as HTMLElement).closest('header')) {
+          this.closeNavbar();
+      }
+  }
+  
+  activateClickOutsideListener() {
+    this.clickOutsideListener = this.onClick.bind(this);
+    document.addEventListener('click', this.clickOutsideListener);
+  }
+
+  deactivateClickOutsideListener() {
+    document.removeEventListener('click', this.clickOutsideListener);
+  }
 }
