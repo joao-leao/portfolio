1d4199023238ef489c2915cb72c77979cb54a8e7
joao-leao <flocoswhite@gmail.com>
2024-05-14 02:38:55 -0300

    fix: accessibility

--- src/app/skills.ts
+++ src/app/skills.ts
@@ -3,14 +3,15 @@ export interface ISkills {
     img: string;
     score: string;
     color: string;
+    descImg: string;
 }
 
 export const skills: ISkills[] = [
-    {name: "HTML", img: "./assets/imgs/html-1.svg", score: "80", color: "orange"},
-    {name: "CSS", img: "./assets/imgs/css-3.svg", score: "80", color: "blue"},
-    {name: "JAVASCRIPT", img: "./assets/imgs/logo-javascript.svg", score: "60", color: "yelow"},
-    {name: "ANGULAR", img: "./assets/imgs/angular-icon-1.svg", score: "50", color: "red"},
-    {name: "GIT", img: "./assets/imgs/git-icon.svg", score: "32", color: "orange-2" },
-    {name: "GITHUB", img: "./assets/imgs/github5.svg", score: "30", color: "black"},
-    {name: "JAVA", img: "./assets/imgs/java2.svg", score: "30", color: "light-blue"}
+    {name: "HTML", img: "./assets/imgs/html-1.svg", score: "80", color: "orange", descImg: "Ícone HTML"},
+    {name: "CSS", img: "./assets/imgs/css-3.svg", score: "80", color: "blue", descImg: "Ícone CSS"},
+    {name: "JAVASCRIPT", img: "./assets/imgs/logo-javascript.svg", score: "60", color: "yelow", descImg: "Ícone Javascript"},
+    {name: "ANGULAR", img: "./assets/imgs/angular-icon-1.svg", score: "50", color: "red", descImg: "Ícone Angular"},
+    {name: "GIT", img: "./assets/imgs/git-icon.svg", score: "32", color: "orange-2", descImg: "Ícone Git" },
+    {name: "GITHUB", img: "./assets/imgs/github5.svg", score: "30", color: "black", descImg: "Ícone GitHub"},
+    {name: "JAVA", img: "./assets/imgs/java2.svg", score: "30", color: "light-blue", descImg: "Ícone Java"}
 ]
