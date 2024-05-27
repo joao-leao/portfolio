62470004e4f8a7f05db10aa07ae8afe0fb1cf10b
joao-leao <flocoswhite@gmail.com>
2024-05-27 18:53:26 -0300

    style: I modified the skills components

--- src/app/skills.ts
+++ src/app/skills.ts
@@ -1,17 +1,17 @@
 export interface ISkills {
     name: string;
     img: string;
-    score: string;
-    color: string;
     descImg: string;
 }
 
 export const skills: ISkills[] = [
-    {name: "HTML", img: "./assets/imgs/html-1.svg", score: "80", color: "orange", descImg: "Ícone HTML"},
-    {name: "CSS", img: "./assets/imgs/css-3.svg", score: "80", color: "blue", descImg: "Ícone CSS"},
-    {name: "JAVASCRIPT", img: "./assets/imgs/logo-javascript.svg", score: "60", color: "yelow", descImg: "Ícone Javascript"},
-    {name: "ANGULAR", img: "./assets/imgs/angular-icon-1.svg", score: "50", color: "red", descImg: "Ícone Angular"},
-    {name: "GIT", img: "./assets/imgs/git-icon.svg", score: "32", color: "orange-2", descImg: "Ícone Git" },
-    {name: "GITHUB", img: "./assets/imgs/github5.svg", score: "30", color: "black", descImg: "Ícone GitHub"},
-    {name: "JAVA", img: "./assets/imgs/java2.svg", score: "30", color: "light-blue", descImg: "Ícone Java"}
+    {name: "HTML", img: "./assets/imgs/html-1.svg", descImg: "Ícone HTML"},
+    {name: "CSS", img: "./assets/imgs/css-3.svg", descImg: "Ícone CSS"},
+    {name: "Javascript", img: "./assets/imgs/logo-javascript.svg", descImg: "Ícone Javascript"},
+    {name: "Angular", img: "./assets/imgs/angular-icon-1.svg", descImg: "Ícone Angular"},
+    {name: "Git", img: "./assets/imgs/git-icon.svg", descImg: "Ícone Git" },
+    {name: "GitHub", img: "./assets/imgs/github5.svg", descImg: "Ícone GitHub"},
+    {name: "Java", img: "./assets/imgs/java2.svg", descImg: "Ícone Java"},
+    {name: "Sass", img: "./assets/imgs/sass.svg", descImg: "Ícone Sass"},
+    {name: "Typescript", img: "./assets/imgs/typescript.svg", descImg: "Ícone Typescript"}
 ]
